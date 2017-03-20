a5f5e724366cb40daae8fe5f41714f132258de62
DeXel <dexel1993@gmail.com>
2017-03-20 01:54:49 -0700

    DEV

--- app/src/main/java/com/example/dexel/camera/MainActivity.java
+++ app/src/main/java/com/example/dexel/camera/MainActivity.java
@@ -1,32 +1,162 @@
 package com.example.dexel.camera;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.graphics.SurfaceTexture;
 import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraCaptureSession;
+import android.hardware.camera2.CameraDevice;
+import android.hardware.camera2.CameraManager;
+import android.hardware.camera2.CaptureRequest;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.Surface;
+import android.view.TextureView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     private final static String TAG = MainActivity.class.getSimpleName();
+    private CameraDevice c = null;
+    private Context mContext = this;
+    private CameraDevice cameraDevice = null;
+    Surface surface = null;
+    TextureView textureView = null;
 
+    private final static int MY_PERMISSIONS_CAMERA_PERMISSIONS = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textureView = (TextureView) findViewById(R.id.textureView);
+        textureView.setSurfaceTextureListener( new TextureView.SurfaceTextureListener() {
+            @Override
+            public void onSurfaceTextureAvailable(SurfaceTexture surface, int width, int height) {
+                //open your camera here
+                openCamera();
+            }
+            @Override
+            public void onSurfaceTextureSizeChanged(SurfaceTexture surface, int width, int height) {
+                // Transform you image captured size according to the surface width and height
+            }
+            @Override
+            public boolean onSurfaceTextureDestroyed(SurfaceTexture surface) {
+                return false;
+            }
+            @Override
+            public void onSurfaceTextureUpdated(SurfaceTexture surface) {
+            }
+        });
+    }
+
+    private final CameraDevice.StateCallback stateCallback = new CameraDevice.StateCallback() {
+        @Override
+        public void onOpened(@NonNull CameraDevice camera) {
+            Log.d(TAG, "CameraState callback onOpened is called");
+            cameraDevice = camera;
+            createPreview();
+        }
+
+        @Override
+        public void onDisconnected(@NonNull CameraDevice camera) {
+            Log.d(TAG, "CameraState callback onOpened is called");
+        }
+
+        @Override
+        public void onError(@NonNull CameraDevice camera, int error) {
+            Log.d(TAG, "CameraState callback onOpened is called");
+        }
+    };
 
 
-        CameraHandler cm = new CameraHandler(this);
+
+    private void openCamera() {
+        CameraManager cm = (CameraManager) mContext.getSystemService(Context.CAMERA_SERVICE);
+
+        if (ActivityCompat.checkSelfPermission((Activity) mContext, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions((Activity) mContext,
+                    new String[]{Manifest.permission.CAMERA},
+                    MY_PERMISSIONS_CAMERA_PERMISSIONS);
+            return;
+        }
+
+
         try {
-            cm.openCamera();
+            String[] cameras = cm.getCameraIdList();
+            List<String> a = new ArrayList<String>(Arrays.asList(cameras));
+            cm.openCamera(cameras[0], stateCallback, null);
+            Log.d("Cameras", a.toString());
         } catch (CameraAccessException e) {
             e.printStackTrace();
         }
 
+    }
+
+    private void displayPreview(CameraCaptureSession session) throws CameraAccessException {
+        CaptureRequest.Builder captureRequestBuilder = cameraDevice.createCaptureRequest(CameraDevice.TEMPLATE_PREVIEW);
+        captureRequestBuilder.addTarget(surface);
+        try {
+            session.setRepeatingRequest(captureRequestBuilder.build(), null, null);
+        } catch (CameraAccessException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void createPreview(){
+
+        SurfaceTexture surfaceTexture = textureView.getSurfaceTexture();
+        surfaceTexture.setDefaultBufferSize(500,500);
+        surface = new Surface(surfaceTexture);
+        try {
+            cameraDevice.createCaptureSession(Arrays.asList(surface), new CameraCaptureSession.StateCallback() {
+                @Override
+                public void onConfigured(@NonNull CameraCaptureSession session) {
+                    //display preview
+                    try {
+                        displayPreview(session);
+                    } catch (CameraAccessException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+                @Override
+                public void onConfigureFailed(@NonNull CameraCaptureSession session) {
+                    Log.e(TAG, "configuring preview failed");
+                }
+            }, null);
+        } catch (CameraAccessException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        switch(requestCode){
+            case MY_PERMISSIONS_CAMERA_PERMISSIONS: {
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_DENIED) {
+                    Toast.makeText(mContext, "Permission required", Toast.LENGTH_SHORT).show();
+                    finish();
+                }
+                return;
+            }
+        }
 
     }
 
 
 
-        /**
+
+    /**
          * A native method that is implemented by the 'native-lib' native library,
          * which is packaged with this application.
          */
